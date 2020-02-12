06773141d4cc7e95a732404151d320d7bfb5ec08
dionybusker <99057959@mydavinci.nl>
2020-02-12 09:16:24 +0100

    files toegevoegd

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <title>B3W1 - lab 2</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+        <script src="js/script.js"></script>
+    </body>
+</html>
